2f72dd058e74f19252ae07c79e40bdf99b7ea72f
PitchapoP <118091873+PitchapoP@users.noreply.github.com>
2022-11-13 15:09:31 +0700

    Create Exercise5_1_Pitchaporn_V.py

---
+++ Exercise5_1_Pitchaporn_V.py
@@ -0,0 +1,9 @@
+# input from user
+n1 = float(input('n1 : '))
+n2 = float(input('n2 : '))
+
+# output
+print(f'\n{n1} + {n2} = {n1+n2}')
+print(f'{n1} - {n2} = {n1-n2}')
+print(f'{n1} * {n2} = {n1*n2}')
+print(f'{n1} / {n2} = {n1/n2}')
