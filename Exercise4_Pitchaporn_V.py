5a2f774d270c82e28a8d86571c0460cc0c4c085a
PitchapoP <118091873+PitchapoP@users.noreply.github.com>
2022-11-13 12:17:53 +0700

    Create Exercise4_Pitchaporn_V.py

---
+++ Exercise4_Pitchaporn_V.py
@@ -0,0 +1,12 @@
+# Variables
+foundEng = 80.2
+generalBus = 76.5
+introToComSys = 78.9
+comPro = 90.1
+
+# print
+print('--- Your Score ---')
+print('Foundation English :', foundEng)
+print('General Business :', generalBus)
+print('Introduction to Computer Systems :', introToComSys)
+print('Computer Programming :', comPro)
