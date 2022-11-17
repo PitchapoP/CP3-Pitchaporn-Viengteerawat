ca6f16d741792c5f7620bb2d94975f475c8871da
PitchapoP <118091873+PitchapoP@users.noreply.github.com>
2022-11-17 09:42:15 +0700

    Create Lecture50_Pitchaporn_V.py

---
+++ Lecture50_Pitchaporn_V.py
@@ -0,0 +1,16 @@
+def addNumber(x, y):
+  print(x, '+', y, '=', x+y)
+
+def subtractNumber(x, y):
+  print(x, '-', y, '=', x-y)
+
+def multiplyNumber(x, y):
+  print(x, 'x', y, '=', x*y)
+
+def divideNumber(x, y):
+  print(x, '/', y, '=', x/y)
+
+addNumber(100,25)
+subtractNumber(100,25)
+multiplyNumber(100,25)
+divideNumber(100,25)
